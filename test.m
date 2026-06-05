3c2a02a2396bfd006dfea73f88b8911bf7e5d46d
mkrzyzowska <maria.krzyzowska113@gmail.com>
2026-06-05 17:19:13 +0200

    Deleted 2016 dir
    test test test

--- test.m
+++
@@ -1,8 +0,0 @@
-baseDir = fullfile(pwd, "2016");
-testFile = fullfile(baseDir, "06", "onemin-Canopy-2016-06-01.csv");
-
-lines = readlines(testFile);
-
-for i = 1:min(30, numel(lines))
-    fprintf("%2d: %s\n", i, lines(i));
-end
